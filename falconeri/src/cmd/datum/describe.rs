5de6e881133c2db387c39d67129ee214f5386c54
Eric Kidd <git@randomhacks.net>
2018-07-14 18:48:47 -0400

    Test `datum describe` template rendering

--- falconeri/src/cmd/datum/describe.rs
+++ falconeri/src/cmd/datum/describe.rs
@@ -8,6 +8,13 @@ use description::render_description;
 /// Template for human-readable `describe` output.
 const DESCRIBE_TEMPLATE: &str = include_str!("describe.txt.hbs");
 
+/// Template parameters.
+#[derive(Serialize)]
+struct Params {
+    datum: Datum,
+    input_files: Vec<InputFile>,
+}
+
 /// Run the `datum describe` subcommand.
 pub fn run(id: Uuid) -> Result<()> {
     // Look up our data in the database.
@@ -16,14 +23,20 @@ pub fn run(id: Uuid) -> Result<()> {
     let input_files = datum.input_files(&conn)?;
 
     // Package into a params object.
-    #[derive(Serialize)]
-    struct Params {
-        datum: Datum,
-        input_files: Vec<InputFile>,
-    }
     let params = Params { datum, input_files };
 
     // Print the description.
     print!("{}", render_description(DESCRIBE_TEMPLATE, &params)?);
     Ok(())
+}
+
+#[test]
+fn render_template() {
+    let job = Job::factory();
+    let datum = Datum::factory(&job);
+    let input_file = InputFile::factory(&datum);
+    let input_files = vec![input_file];
+    let params = Params { datum, input_files };
+    render_description(DESCRIBE_TEMPLATE, &params)
+        .expect("could not render template");
 }
