eb7a443c4a06a873a3db5eae1f4f86286a883eca
Seamus Abshere <seamus@abshere.net>
2019-06-14 11:07:15 -0400

    re-using client connection doesn't work, at least across falc proxy

--- falconeri/src/cmd/job/retry.rs
+++ falconeri/src/cmd/job/retry.rs
@@ -6,7 +6,8 @@ use falconeri_common::{prelude::*, rest_api::Client};
 pub fn run(job_name: &str) -> Result<()> {
     let client = Client::new(ConnectVia::Proxy)?;
     let job = client.find_job_by_name(job_name)?;
-    let new_job = client.retry_job(&job)?;
+    let client2 = Client::new(ConnectVia::Proxy)?;
+    let new_job = client2.retry_job(&job)?;
     println!("{}", new_job.job_name);
     Ok(())
 }
