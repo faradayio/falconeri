cc99f09edf3e339d8cf6df2b0f078c4eaecde7ad
Eric Kidd <git@randomhacks.net>
2019-06-17 10:50:06 -0400

    retry: Explain why we need a second client for now
    This is a limitation in our `Client` type and in how we handle `retry`.
    We should fix `retry` to be truly idempotent on the server (by having
    the client send a UUID with each request that we can use to detect
    duplicate requests), and then wrap the client in a retry block.

--- falconeri/src/cmd/job/retry.rs
+++ falconeri/src/cmd/job/retry.rs
@@ -4,10 +4,13 @@ use falconeri_common::{prelude::*, rest_api::Client};
 
 /// The `job retry` subcommand.
 pub fn run(job_name: &str) -> Result<()> {
-    let client = Client::new(ConnectVia::Proxy)?;
+    let mut client = Client::new(ConnectVia::Proxy)?;
     let job = client.find_job_by_name(job_name)?;
-    let client2 = Client::new(ConnectVia::Proxy)?;
-    let new_job = client2.retry_job(&job)?;
+    // TODO: We need to create a new client here because we don't have HTTP
+    // keepalive set up, and we don't have a good way to run `retry_job`
+    // idempotently yet.
+    client = Client::new(ConnectVia::Proxy)?;
+    let new_job = client.retry_job(&job)?;
     println!("{}", new_job.job_name);
     Ok(())
 }
